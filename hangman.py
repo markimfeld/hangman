e382daa1043643fde3c8707cbddcf1930306330c
markimfeld <sebastianimfeld@gmail.com>
2020-02-13 18:29:05 -0300

    init main while

--- hangman.py
+++ hangman.py
@@ -62,39 +62,49 @@ WORDS = [
     'django'
 ]
 
-PLAYER_LIFE = 0
-
 def getSecretWord():
     secretWord = random.choice(WORDS)
     return secretWord
 
-def showDrawing():
-    if PLAYER_LIFE == 6:
+def showDrawing(playerLife):
+    if playerLife == 6:
         print(HANGMAN_PICS[0])
-    elif PLAYER_LIFE == 5:
+    elif playerLife == 5:
         print(HANGMAN_PICS[1])
-    elif PLAYER_LIFE == 4:
+    elif playerLife == 4:
         print(HANGMAN_PICS[2])
-    elif PLAYER_LIFE == 3:
+    elif playerLife == 3:
         print(HANGMAN_PICS[3])
-    elif PLAYER_LIFE == 2:
+    elif playerLife == 2:
         print(HANGMAN_PICS[4])
-    elif PLAYER_LIFE == 1:
+    elif playerLife == 1:
         print(HANGMAN_PICS[5])
     else:
         print(HANGMAN_PICS[6])
 
 
 def showBlank(secretWord, letter):
-    nLetter = secretWord.__len__()
     
-    splittedWord = secretWord.split(letter)
+    newSplittedWord = '_'*len(secretWord)
 
-    for item in range(len(splittedWord)):
-        splittedWord[item] = '_'*len(splittedWord[item])       
+    if letter in secretWord:
+        splittedWord = secretWord.split(letter)
 
-    newSplittedWord = letter.join(splittedWord)
+        for item in range(len(splittedWord)):
+            splittedWord[item] = '_'*len(splittedWord[item])       
 
-    print(newSplittedWord)
+        newSplittedWord = letter.join(splittedWord)
 
-showBlank('tictac','a')
+    return newSplittedWord
+    
+
+playAgain = 'yes'
+
+while playAgain == 'yes' or playAgain == 'Yes':
+    playerLife = 6
+    guessWord = getSecretWord()
+    showDrawing(playerLife)
+    print("- Secret Word: " + showBlank(guessWord, letter=','))
+
+    print('\n\nGuess a letter: ')
+    letter = input()
