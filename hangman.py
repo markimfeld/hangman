2f9d78614f4f915e15716456dd6a7b428f6764cf
marcos <sebastianimfeld@gmail.com>
2020-02-17 10:19:31 -0300

    testing

--- hangman.py
+++ hangman.py
@@ -72,7 +72,7 @@ coyote crow deer dog donkey duck eagle ferret fox frog goat goose hawk
 lion lizard llama mole monkey moose mouse mule newt otter owl panda
 parrot pigeon python rabbit ram rat raven rhino salmon seal shark sheep
 skunk sloth snake spider stork swan tiger toad trout turkey turtle
-weasel whale wolf wombat zebra kiko tito kanguro pincel piano etc'''.split()
+weasel whale wolf wombat zebra'''.split()
 
 def getRandomWord(wordList):
     wordIndex = random.randint(0, len(wordList) - 1)
